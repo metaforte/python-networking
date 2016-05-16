da1f5cb0fc13deb441c764954adbb67a0ffa5b78
Manas Marthi <mlnsastry@gmail.com>
2016-05-16 13:45:12 +0100

    Create socket_module.py

---
+++ socket_module.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+import socket
+
+
+# Pring your machine name and IP Address
+# Import socket module
+# use the gethostname() function
+def print_hostname():
+  my_hostname=socket.gethostname()
+  
+if __name__ == __main__:
+  print_hostname()
